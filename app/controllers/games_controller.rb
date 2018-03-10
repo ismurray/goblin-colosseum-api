dabeedb837ba6d55af63c3123d3c81ec0db7aad3
ismurray <ians.murray@yahoo.com>
2018-03-10 10:39:42 -0500

    Add round col to Games table
    - Generate migration using command
     'rails g migration add_round_to_games round:integer'
    - Add :round to GameController game_params whitelist
    - Add :round to game_serializer
    
    The round col stores the current round number of a game.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -47,6 +47,7 @@ class GamesController < ProtectedController
 
     # Only allow a trusted parameter "white list" through.
     def game_params
-      params.require(:game).permit(:over, :score, :user_id, :player_state, :goblin_state)
+      params.require(:game).permit(:over, :score, :user_id, :player_state,
+                                   :goblin_state, :round)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180309145149) do
+ActiveRecord::Schema.define(version: 20180310151128) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180309145149) do
     t.bigint "user_id"
     t.text "player_state"
     t.text "goblin_state"
+    t.integer "round"
     t.index ["user_id"], name: "index_games_on_user_id"
   end
 
